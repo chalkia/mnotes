6e25698fda46e899be2281f7d48b32852864e678
chalkia <chalkia.duck@gmail.com>
2026-02-05 10:29:19 +0200

    Update supabase-client.js  Κeys Secured

--- js/supabase-client.js
+++ js/supabase-client.js
@@ -2,8 +2,8 @@
    SUPABASE CLIENT BRIDGE
    ========================================= */
 // !!! ΠΡΟΣΟΧΗ: ΒΑΛΕ ΤΑ ΔΙΚΑ ΣΟΥ ΚΛΕΙΔΙΑ ΕΔΩ !!!
-const SUPABASE_URL = 'https://ihrckneywnzgkxantrvm.supabase.co'; 
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlocmNrbmV5d256Z2t4YW50cnZtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAxMTU4NzAsImV4cCI6MjA4NTY5MTg3MH0.Gj7UdQebw8Jg6XbpfZxehPgyikhoUGG1MRd181EXztw'; 
+const SUPABASE_URL = CONFIG.SUPABASE_URL;
+const SUPABASE_KEY = CONFIG.SUPABASE_KEY; 
 
 // ΑΛΛΑΓΗ ΟΝΟΜΑΤΟΣ: Από 'supabase' σε 'supabaseClient' για να μην τσακώνεται με τη βιβλιοθήκη
 let supabaseClient = null;

47942f9236b29e2d59381fdef064e6dab140395a
chalkia <chalkia.duck@gmail.com>
2026-04-05 11:58:38 +0300

    Update sw.js  version 0.5.0

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mnotes-ver0.28.0'; // Ανέβασα την έκδοση για να πάρει την αλλαγή
+const CACHE_NAME = 'mnotes-ver0.5.0'; // Ανέβασα την έκδοση για να πάρει την αλλαγή
 const ASSETS_TO_CACHE = [
     './',
     'index.html',
